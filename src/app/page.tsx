913652e1d545718ef3b6bd7f370923063effd11f
Emmett Hintz <eahintz@colgate.edu>
2024-09-28 15:40:26 -0400

    Test deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import {
 } from "lucide-react";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 
+
 export default function LandingPage() {
   return (
     <div className="min-h-screen bg-white text-[#6A7573]">
